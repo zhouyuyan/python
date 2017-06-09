c4bdfa62438fe19113897ed63db92c917ce2f356
zhouyuyan <791430360@qq.com>
2017-06-09 14:11:17 +0800

    优化分享功能

--- TestCase/test_my.py
+++ TestCase/test_my.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 # Created by zhouyuyan on 2017/5/19 14:35
 import unittest
-
-from Common.action import ElementActions
 from Common.yunxiCommon import *
 from Common.Element import *
 from appium import webdriver as appdriver
@@ -18,9 +16,6 @@ class My(unittest.TestCase):
         self.driver = appdriver.Remote('http://localhost:4723/wd/hub', desired_caps(self))
 
         self.driver.implicitly_wait(5)
-        writeLog(self)
-        global action
-        action = ElementActions(driver=self.driver)
 
         print("----------------setup-------------")
 
@@ -33,13 +28,20 @@ class My(unittest.TestCase):
         """测试登录-设置（清除缓存）"""
         goto_login(self)
         login(self, '13071825896', '3279')
+        self.goto_setting()
         self.setting()
+
+    def test_f(self):
+        """测试登录-设置（意见反馈）"""
+        goto_login(self)
+        login(self, '13071825896', '3279')
+        self.goto_setting()
+        self.feedback()
 
     def setting(self):
 
         try:
-            get_id(self, 'tv.yunxi.app:id/rl_setting').click()
-            L.i('--点击设置按钮--')
+
             sleep(2)
             get_id(self, 'tv.yunxi.app:id/rl_wipe_cache').click()
             L.i('--点击清空缓存按钮--')
@@ -55,6 +57,23 @@ class My(unittest.TestCase):
             L.w('--操作失败--')
 
         sleep(2)
+
+    def goto_setting(self):
+        get_id(self, 'tv.yunxi.app:id/rl_setting').click()
+        L.i('--点击设置按钮--')
+
+    def feedback(self):
+        try:
+            feedback = get_xpath(self, "//android.widget.EditText[@text='请简要描述您的问题和意见']")
+            re = lambda action: action.is_element_displayed(feedback)
+            if re:
+                feedback.send_keys("hahhahahahahahahahhahaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+                sleep(1)
+            get_xpath(self, "//android.widget.EditText[@text='请留下邮箱方便我们联系您']").send_keys("11@qq.com")
+            sleep(1)
+            get_xpath(self, "//android.view.View[@content-desc='提交']").click()
+        except:
+            print("提交反馈失败")
 
 
 if __name__ == '__My__':
